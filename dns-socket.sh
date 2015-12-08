8b1c50f6e6bfbb23de34b14e7c82be6a3ea7f405
t0r0t0r0 <dd_ybb@yahoo.co.jp>
2015-12-08 13:45:18 +0900

    Delete dns-socket.sh

--- dns-socket.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-
-outputudpsocket=`netstat -anu|awk '{print $5" "$6}'|grep ":53 "|wc -l`
-outputtcpsocket=`netstat -ant|awk '{print $5" "$6}'|grep ":53 "|wc -l`
-
-echo "outputudpsocket:${outputudpsocket} outputtcpsocket:${outputtcpsocket}"
